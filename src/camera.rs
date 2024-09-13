67021114cb5da27643cba0d466dd823e413d68bc
nyushiri <nyushiri@gmail.com>
2024-09-14 00:02:28 +0300

    camera follow

--- src/camera.rs
+++ src/camera.rs
@@ -1,12 +1,17 @@
 use bevy::prelude::*;
 
+use crate::player::Player;
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_camera);
+        app.add_systems(FixedUpdate, sync_player_camera);
     }
 }
+
+#[derive(Component)]
+struct Camera;
 
 fn spawn_camera(
     mut commands: Commands,
@@ -18,5 +23,16 @@ fn spawn_camera(
             ..default()
         },
         ..default()
-    }); 
+    }).insert(Camera); 
+}
+
+fn sync_player_camera(
+    mut player_query: Query<&Transform, (With<Player>, Without<Camera>)>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+) {
+    if let Ok(mut camera_transform) = camera_query.get_single_mut() {
+        if let Ok(player_transform) = player_query.get_single_mut() {
+            camera_transform.translation = Vec3::new(player_transform.translation.x, player_transform.translation.y, 2.0);
+        }
+    }
 }
