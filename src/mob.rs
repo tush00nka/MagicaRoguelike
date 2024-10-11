f29ce0d749ba7476f98753b02fde1424a6b7bdb8
Nrastamann <nicita351@Yandex.ru>
2024-10-12 01:30:30 +0300

    Now projectiles damage player and get destroyed after that.

--- src/mob.rs
+++ src/mob.rs
@@ -14,7 +14,7 @@ use crate::{
     level_completion::PortalEvent,
     pathfinding::Pathfinder,
     player::{Player, PlayerDeathEvent},
-    projectile::{Friendly, Projectile, SpawnProjectileEvent},
+    projectile::{Friendly, Hostile, Projectile, SpawnProjectileEvent},
     GameLayer,
     GameState,
 };
@@ -31,7 +31,7 @@ impl Plugin for MobPlugin {
             .add_systems(OnEnter(GameState::InGame), debug_spawn_mobs)
             .add_systems(
                 FixedUpdate,
-                (move_mobs, hit_projectiles, hit_player, mob_death, teleport_mobs, mob_shoot).run_if(in_state(GameState::InGame)),
+                (move_mobs, hit_projectiles, hit_player, mob_death, teleport_mobs, mob_shoot, proj_hit_player).run_if(in_state(GameState::InGame)),
             );
     }
 }
@@ -382,6 +382,39 @@ fn hit_player(
                     if health.current <= 0 {
                         ev_death.send(PlayerDeathEvent(player_e));
                     }
+                }
+            }
+        }
+    }
+}
+
+fn proj_hit_player(
+    mut commands: Commands,
+    mut collision_event_reader: EventReader<Collision>,
+    projectile_query: Query<(Entity, &Projectile), With<Hostile>>,
+    mut player_query: Query<(Entity, &mut Health, &Player), Without<Invincibility>>,
+    mut ev_hp: EventWriter<PlayerHPChanged>,
+    mut ev_death: EventWriter<PlayerDeathEvent>,
+) {
+    for Collision(contacts) in collision_event_reader.read() {
+        let mut projectile_e = Entity::PLACEHOLDER;
+
+        if projectile_query.contains(contacts.entity1) && player_query.contains(contacts.entity2) {
+            projectile_e = contacts.entity1;
+        } else if projectile_query.contains(contacts.entity2) && player_query.contains(contacts.entity1) {
+            projectile_e = contacts.entity2;
+        }
+
+        if let Ok((player_e, mut health, player)) = player_query.get_single_mut() {
+            for (proj_cand_e, proj) in projectile_query.iter() {
+                if proj_cand_e == projectile_e {
+                    health.damage(proj.damage as i32);
+                    ev_hp.send(PlayerHPChanged);
+                    commands.entity(player_e).insert(Invincibility::new(player.invincibility_time));
+                    if health.current <= 0 {
+                        ev_death.send(PlayerDeathEvent(player_e));
+                    }
+                    commands.get_entity(proj_cand_e).unwrap().despawn();
                 }
             }
         }
