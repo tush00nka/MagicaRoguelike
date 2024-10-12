3182a6727fb218d8d05f6c0580db34aa3421498d
Nrastamann <nicita351@Yandex.ru>
2024-10-13 02:22:38 +0300

    Now there are a bunch of bundles for mobs.

--- src/mob.rs
+++ src/mob.rs
@@ -3,48 +3,41 @@ use std::{f32::consts::PI, time::Duration};
 
 use avian2d::prelude::*;
 use bevy::prelude::*;
+use dynamics::rigid_body;
 use rand::Rng;
 
 use crate::{
-    animation::AnimationConfig,
     exp_orb::SpawnExpOrbEvent,
-    experience::PlayerExperience, 
-    gamemap::{
-        LevelGenerator,
-        MobMap,
-        TileType,
-        ROOM_SIZE
-    },
+    experience::PlayerExperience,
+    gamemap::{LevelGenerator, MobMap, TileType, ROOM_SIZE},
     health::Health,
-    level_completion::{
-        PortalEvent,
-        PortalManager
-    },
+    level_completion::{PortalEvent, PortalManager},
     pathfinding::Pathfinder,
     player::Player,
-    projectile::{
-        Friendly,
-        Projectile,
-        SpawnProjectileEvent
-    },
+    projectile::SpawnProjectileEvent,
+    projectile::{Friendly, Projectile},
     stun::Stun,
-    GameLayer,
-    GameState
+    GameLayer, GameState,
 };
 
 pub struct MobPlugin;
 
 impl Plugin for MobPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .insert_resource(MobMap::default())
+        app.insert_resource(MobMap::default())
             .add_event::<MobDeathEvent>()
             .add_systems(OnEnter(GameState::InGame), spawn_mobs)
             .add_systems(
                 FixedUpdate,
-                (move_mobs, hit_projectiles, teleport_mobs, mob_shoot).run_if(in_state(GameState::InGame)),
-            )
-            .add_systems(Update, (animate_mobs, mob_death).run_if(in_state(GameState::InGame)));
+                (
+                    move_mobs,
+                    hit_projectiles,
+                    mob_death,
+                    teleport_mobs,
+                    mob_shoot,
+                )
+                    .run_if(in_state(GameState::InGame)),
+            );
     }
 }
 pub enum MobType {
@@ -52,14 +45,84 @@ pub enum MobType {
     Teleport,
 }
 
-#[derive(Component)]
-pub struct Teleport {
-    pub amount_of_tiles: u8,
+#[derive(Bundle)]
+pub struct PhysicBundle {
+    collider: Collider,
+    axes: LockedAxes,
+    gravity: GravityScale,
+    collision_layers: CollisionLayers,
+    linear_velocity: LinearVelocity,
+}
+
+#[derive(Bundle)]
+pub struct MobBundle {
+    texture_path: String,
+    mob_type: MobType,
+    mob: Mob,
+    loot: MobLoot,
+    body_type: RigidBody,
+    health: Health,
+}
+
+#[derive(Bundle)]
+pub struct FireMageBundle {
+    teleport_ability: Teleport,
+    shoot_ability: ShootAbility,
+}
+impl Default for FireMageBundle {
+    fn default(timer: Range<u64>) -> Self {//need to have 1 same
+        Self {
+            teleport_ability: Teleport {
+                amount_of_tiles: 4,
+                place_to_teleport: vec![],
+                time_to_teleport: Timer::new(
+                    Duration::from_millis(rand::thread_rng().gen_range(timer.clone())),
+                    TimerMode::Repeating,
+                ),
+            },
+            shoot_ability: ShootAbility { time_to_shoot: () },
+        }
+    }
+}
+impl MobBundle {
+    fn mossling(i: usize, j: usize) -> Self {
+        Self {
+            texture_path: "textures/mob_mossling.png".to_string(),
+            mob_type: MobType::Mossling,
+            mob: Mob { damage: 20 },
+            loot: MobLoot { orbs: 3 },
+            body_type: RigidBody::Dynamic,
+            health: Health {
+                max: 100,
+                current: 100,
+            },
+        }
+    }
+    fn fire_mage(i: usize, j: usize) -> Self {
+        Self {
+            texture_path: "textures/fire_mage.png".to_string(),
+            mob_type: MobType::Mossling,
+            mob: Mob { damage: 20 },
+            loot: MobLoot { orbs: 3 },
+            body_type: RigidBody::Kinematic,
+            health: Health {
+                max: 80,
+                current: 80,
+            },
+        }
+    }
 }
 
 #[derive(Component)]
-pub struct ShootAbility{
-    pub time_to_shoot: Timer
+pub struct Teleport {
+    pub amount_of_tiles: u8,
+    pub place_to_teleport: Vec<(u16, u16)>,
+    pub time_to_teleport: Timer,
+}
+
+#[derive(Component)]
+pub struct ShootAbility {
+    pub time_to_shoot: Timer,
 }
 #[derive(Component)]
 pub struct Mob {
@@ -89,10 +152,9 @@ pub struct MobDeathEvent {
     pub dir: Vec3,
 }
 
-pub fn spawn_mobs(
+pub fn pick_mob_to_spawn(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
     room: Res<LevelGenerator>,
     mut mob_map: ResMut<MobMap>,
 ) {
@@ -103,8 +165,9 @@ pub fn spawn_mobs(
             if grid[i][j] == TileType::Floor {
                 let mut rng = rand::thread_rng();
                 //need to fix 0 mob levels
-                if rng.gen::<f32>() > 0.9 && (i > 18 || i < 14) && (j > 18 || j < 14){ // make sure emenies don't spawn too close to player (todo: rewrite)
-                    
+                if rng.gen::<f32>() > 0.9 && (i > 18 || i < 14) && (j > 18 || j < 14) {
+                    // make sure emenies don't spawn too close to player (todo: rewrite)
+
                     let mob_type: MobType = rand::random();
                     let texture_path: &str;
                     let mut can_shoot: bool = false;
@@ -112,39 +175,23 @@ pub fn spawn_mobs(
                     let mut amount_of_tiles: u8 = 0;
                     let timer: std::ops::Range<u64>;
 
-                    let frame_count: u32;
-                    let fps: u8;
-
                     match mob_type {
                         MobType::Mossling => {
-                            texture_path = "textures/mobs/mossling.png";
+                            texture_path = "textures/mob_mossling.png";
                             timer = 500..999;
-
-                            frame_count = 4;
-                            fps = 12;
                         }
                         MobType::Teleport => {
                             timer = 3000..5000;
                             amount_of_tiles = 4;
                             has_teleport = true;
                             can_shoot = true;
-                            texture_path = "textures/mobs/fire_mage.png";
-
-                            frame_count = 2;
-                            fps = 3;
+                            texture_path = "textures/mob_teleport_placeholder.png"
                         }
                     }
-                  
-                    let texture = asset_server.load(texture_path);
-
-                    let layout = TextureAtlasLayout::from_grid(UVec2::splat(16), frame_count, 1, None, None);
-                    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-                
-                    let animation_config = AnimationConfig::new(0, frame_count as usize - 1, fps);
 
                     let mob = commands
                         .spawn(SpriteBundle {
-                            texture,
+                            texture: asset_server.load(texture_path),
                             transform: Transform::from_xyz(
                                 (i as i32 * ROOM_SIZE) as f32,
                                 (j as i32 * ROOM_SIZE) as f32,
@@ -153,15 +200,6 @@ pub fn spawn_mobs(
                             ..default()
                         })
                         .id();
-
-                    commands.entity(mob)
-                        .insert(
-                            TextureAtlas {
-                                layout: texture_atlas_layout.clone(),
-                                index: animation_config.first_sprite_index,
-                            }
-                        )
-                        .insert(animation_config);
 
                     commands
                         .entity(mob)
@@ -179,9 +217,7 @@ pub fn spawn_mobs(
                             ],
                         ))
                         .insert(LinearVelocity::ZERO)
-                        .insert(Mob { 
-                            damage: 20
-                         })
+                        .insert(Mob { damage: 20 })
                         .insert(Pathfinder {
                             path: vec![],
                             update_path_timer: Timer::new(
@@ -189,7 +225,6 @@ pub fn spawn_mobs(
                                 TimerMode::Repeating,
                             ),
                             speed: 2500.,
-
                         })
                         .insert(MobLoot { orbs: 3 })
                         .insert(Health {
@@ -198,16 +233,22 @@ pub fn spawn_mobs(
                         });
 
                     if has_teleport {
-                        commands.entity(mob).insert(Teleport { amount_of_tiles }).insert(RigidBody::Kinematic);
+                        commands
+                            .entity(mob)
+                            .insert(Teleport { amount_of_tiles })
+                            .insert(RigidBody::Kinematic);
                         mob_map.map[i][j] = mob_id;
                         mob_id += 1;
                     } else {
                         commands.entity(mob).insert(RigidBody::Dynamic);
                     }
-                    if can_shoot{
-                        commands.entity(mob).insert(ShootAbility{time_to_shoot: Timer::new(
-                            Duration::from_millis(rand::thread_rng().gen_range(timer)),
-                            TimerMode::Repeating)});
+                    if can_shoot {
+                        commands.entity(mob).insert(ShootAbility {
+                            time_to_shoot: Timer::new(
+                                Duration::from_millis(rand::thread_rng().gen_range(timer)),
+                                TimerMode::Repeating,
+                            ),
+                        });
                     }
                 }
             }
@@ -215,7 +256,9 @@ pub fn spawn_mobs(
     }
 }
 
-fn teleport_mobs(mut mob_query: Query<(&mut Transform, &mut Pathfinder), (Without<Stun>, With<Teleport>)>) {
+fn teleport_mobs(
+    mut mob_query: Query<(&mut Transform, &mut Pathfinder), (Without<Stun>, With<Teleport>)>,
+) {
     // maybe add time dependency to teleport time? idk
     for (mut transform, mut mob) in mob_query.iter_mut() {
         if mob.path.len() > 0 {
@@ -229,10 +272,15 @@ fn teleport_mobs(mut mob_query: Query<(&mut Transform, &mut Pathfinder), (Withou
     }
 }
 
-fn move_mobs(mut mob_query: Query<(&mut LinearVelocity, &Transform, &mut Pathfinder), (Without<Stun>, Without<Teleport>)>, time: Res<Time>) {
+fn move_mobs(
+    mut mob_query: Query<
+        (&mut LinearVelocity, &Transform, &mut Pathfinder),
+        (Without<Stun>, Without<Teleport>),
+    >,
+    time: Res<Time>,
+) {
     for (mut linvel, transform, mut pathfinder) in mob_query.iter_mut() {
         if pathfinder.path.len() > 0 {
-
             //let mob_tile_pos = Vec2::new(((transform.translation.x - (ROOM_SIZE / 2) as f32) / ROOM_SIZE as f32).floor(), (transform.translation.y - (ROOM_SIZE / 2) as f32) / ROOM_SIZE as f32).floor();
             let direction = Vec2::new(
                 pathfinder.path[0].0 as f32 * 32. - transform.translation.x,
@@ -242,7 +290,11 @@ fn move_mobs(mut mob_query: Query<(&mut LinearVelocity, &Transform, &mut Pathfin
 
             linvel.0 = direction * pathfinder.speed * time.delta_seconds();
 
-            if transform.translation.truncate().distance(Vec2::new(pathfinder.path[0].0 as f32 * 32., pathfinder.path[0].1 as f32 * 32.)) <= 4. {
+            if transform.translation.truncate().distance(Vec2::new(
+                pathfinder.path[0].0 as f32 * 32.,
+                pathfinder.path[0].1 as f32 * 32.,
+            )) <= 4.
+            {
                 pathfinder.path.remove(0);
             }
         }
@@ -254,27 +306,26 @@ fn mob_shoot(
     mut mob_query: Query<(&Transform, &mut ShootAbility)>,
     mut player_query: Query<&Transform, (With<Player>, Without<Mob>)>,
     time: Res<Time>,
-){
-    for (transform, mut can_shoot) in mob_query.iter_mut(){
+) {
+    for (transform, mut can_shoot) in mob_query.iter_mut() {
         if let Ok(player) = player_query.get_single_mut() {
             can_shoot.time_to_shoot.tick(time.delta());
             if can_shoot.time_to_shoot.just_finished() {
                 println!("Start to cast");
-                let dir = (player.translation.truncate() - transform.translation.truncate()).normalize_or_zero();
+                let dir = (player.translation.truncate() - transform.translation.truncate())
+                    .normalize_or_zero();
                 let angle = dir.y.atan2(dir.x);
-                ev_shoot.send(
-                    SpawnProjectileEvent { 
-                        texture_path: "textures/fireball.png".to_string(), 
-                        color:  Color::srgb(2.5, 1.25, 1.0), 
-                        translation: transform.translation, 
-                        angle: angle, 
-                        radius: 8.0, 
-                        speed: 150., 
-                        damage: 20, 
-                        element: crate::elements::ElementType::Fire, 
-                        is_friendly: false
-                    }
-                );
+                ev_shoot.send(SpawnProjectileEvent {
+                    texture_path: "textures/fireball.png".to_string(),
+                    color: Color::srgb(2.5, 1.25, 1.0),
+                    translation: transform.translation,
+                    angle: angle,
+                    radius: 8.0,
+                    speed: 150.,
+                    damage: 20,
+                    element: crate::elements::ElementType::Fire,
+                    is_friendly: false,
+                });
                 println!("sent event");
             }
         }
@@ -310,9 +361,8 @@ fn hit_projectiles(
                 for (proj_candidate_e, projectile, projectile_transform) in projectile_query.iter()
                 {
                     if proj_e.is_some() && proj_e.unwrap() == proj_candidate_e {
-
                         health.damage(projectile.damage.try_into().unwrap());
-                        
+
                         // кидаем стан на моба
                         commands.entity(mob_e.unwrap()).insert(Stun::new(0.5));
 
@@ -349,27 +399,24 @@ fn mob_death(
     mut ev_mob_death: EventReader<MobDeathEvent>,
 ) {
     for ev in ev_mob_death.read() {
-
         portal_manager.set_pos(ev.pos);
         portal_manager.pop_mob();
         if portal_manager.no_mobs_on_level() {
-            ev_spawn_portal.send( PortalEvent{pos: portal_manager.get_pos()});
-        }    
-    
+            ev_spawn_portal.send(PortalEvent {
+                pos: portal_manager.get_pos(),
+            });
+        }
+
         let orb_count = (ev.orbs + player_experience.orb_bonus) as i32;
         let half_count = (orb_count as f32 / 2.).round() as i32;
-    
+
         let offset = PI / 12.;
-        for i in (-orb_count/2)..half_count {
+        for i in (-orb_count / 2)..half_count {
             // считаем точки, куда будем выбрасывать частицы опыта
             let angle = ev.dir.y.atan2(ev.dir.x) + offset * i as f32;
             let direction = Vec2::from_angle(angle) * 32.0;
-            let destination = Vec3::new(
-                ev.pos.x + direction.x,
-                ev.pos.y + direction.y,
-                ev.pos.z,
-            );
-    
+            let destination = Vec3::new(ev.pos.x + direction.x, ev.pos.y + direction.y, ev.pos.z);
+
             ev_spawn_orb.send(SpawnExpOrbEvent {
                 pos: ev.pos,
                 destination,
@@ -377,28 +424,5 @@ fn mob_death(
         }
 
         commands.entity(ev.entity).despawn();
-    }
-}
-
-fn animate_mobs(
-    time: Res<Time>,
-    mut query: Query<(&mut AnimationConfig, &mut TextureAtlas), (With<Mob>, Without<Stun>)>,
-) {
-    for (mut config, mut atlas) in &mut query {
-        // we track how long the current sprite has been displayed for
-        config.frame_timer.tick(time.delta());
-
-        // If it has been displayed for the user-defined amount of time (fps)...
-        if config.frame_timer.just_finished() {
-            if atlas.index == config.last_sprite_index {
-                // ...and it IS the last frame, then we move back to the first frame and stop.
-                atlas.index = config.first_sprite_index;
-            } else {
-                // ...and it is NOT the last frame, then we move to the next frame...
-                atlas.index += 1;
-                // ...and reset the frame timer to start counting all over again
-                config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
-            }
-        }
     }
 }
