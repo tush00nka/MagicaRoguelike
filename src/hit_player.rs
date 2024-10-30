9b4af8279da5b1259bc209e7936b4b7e2a0b951b
tush00nka <endline00@ya.ru>
2024-10-30 14:34:56 +0300

    hit_player reworked

--- src/hit_player.rs
+++ src/hit_player.rs
@@ -23,99 +23,84 @@ pub struct HitPlayerPlugin;
 
 impl Plugin for HitPlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(FixedUpdate, (hit_player, proj_hit_player, damage_player)
+        app.add_systems(Update, (hit_player, proj_hit_player, damage_player)
                 .run_if(in_state(GameState::InGame)));
     }
 }
 //damage by collision with mob
-fn hit_player(  //todo: change that we could add resistance mechanic
-    mut collision_event_reader: EventReader<Collision>,
+fn hit_player(
     mob_query: Query<(Entity, &Mob), Without<Player>>,
-    mut player_query: Query<(Entity, &mut Health, &Player), Without<Invincibility>>,
+    mut player_query: Query<(&CollidingEntities, &mut Health), (With<Player>, Without<Invincibility>)>,
 ) {
-    for Collision(contacts) in collision_event_reader.read() {
-        let mut mob_e = Entity::PLACEHOLDER;
+    let Ok((colliding_e, mut health)) = player_query.get_single_mut() else {
+        return;
+    };
 
-        if mob_query.contains(contacts.entity1) && player_query.contains(contacts.entity2) {
-            mob_e = contacts.entity1;
-        } else if mob_query.contains(contacts.entity2) && player_query.contains(contacts.entity1) {
-            mob_e = contacts.entity2;
-        }
-
-        if let Ok((_player_e, mut health, _player)) = player_query.get_single_mut() {
-            for (mob_cadidate_e, mob) in mob_query.iter() {
-                if mob_cadidate_e == mob_e {
-                    health.hit_queue.push(Hit {
-                        damage: mob.damage,
-                        element: None,
-                        direction: Vec3::ZERO,
-                    });
-                }
-            }
+    for (mob_e, mob) in mob_query.iter() {
+        if colliding_e.contains(&mob_e) {
+            health.hit_queue.push( Hit {
+                damage: mob.damage,
+                element: None,
+                direction: Vec3::ZERO,
+            });
         }
     }
-}
 
+}
 //damage by projectiles
 fn proj_hit_player( //todo: change that we could add resistance mechanic
     mut commands: Commands,
-    mut collision_event_reader: EventReader<Collision>,
     projectile_query: Query<(Entity, &Projectile), With<Hostile>>,
-    mut player_query: Query<(Entity, &mut Health, &Player), Without<Invincibility>>,
+    mut player_query: Query<(&CollidingEntities, &mut Health), (With<Player>, Without<Invincibility>)>,
 ) {
-    for Collision(contacts) in collision_event_reader.read() {
-        let mut projectile_e = Entity::PLACEHOLDER;
+    let Ok((colliding_e, mut health)) = player_query.get_single_mut() else {
+        return;
+    };
 
-        if projectile_query.contains(contacts.entity1) && player_query.contains(contacts.entity2) {
-            projectile_e = contacts.entity1;
-        } else if projectile_query.contains(contacts.entity2)
-            && player_query.contains(contacts.entity1)
-        {
-            projectile_e = contacts.entity2;
-        }
+    for (proj_e, projectile) in projectile_query.iter() {
+        if colliding_e.contains(&proj_e) {
+            health.hit_queue.push( Hit {
+                damage: projectile.damage as i32,
+                element: Some(projectile.element),
+                direction: Vec3::ZERO,
+            });
 
-        if let Ok((_player_e, mut health, _player)) = player_query.get_single_mut() {
-            for (proj_cand_e, proj) in projectile_query.iter() {
-                if proj_cand_e == projectile_e {
-                    health.hit_queue.push( Hit {
-                        damage: proj.damage as i32,
-                        element: Some(proj.element),
-                        direction: Vec3::ZERO,
-                    });
-                    commands.get_entity(proj_cand_e).unwrap().despawn();
-                }
-            }
+            commands.entity(proj_e).despawn();
         }
     }
+
 }
 
 fn damage_player(
     mut commands: Commands,
     mut ev_death: EventWriter<PlayerDeathEvent>,
-    mut player_query: Query<(Entity, &mut Health, &Player, &ElementResistance), With<Player>>,
+    mut player_query: Query<(Entity, &mut Health, &Player, &ElementResistance), (With<Player>, Without<Invincibility>)>,
 ) {
-    for (player_e, mut health, player, resistance) in player_query.iter_mut() {
-        if !health.hit_queue.is_empty() {
-            let hit = health.hit_queue.remove(0);
+    let Ok((player_e, mut health, player, resistance)) = player_query.get_single_mut() else {
+        return;
+    };
 
-            // считаем сопротивление
-            let mut damage = hit.damage;
-            resistance.calculate_for(&mut damage, hit.element);
+    if !health.hit_queue.is_empty() {
+        let hit = health.hit_queue.remove(0);
+        health.hit_queue.clear();
 
-            // наносим урон
-            health.damage(damage);
+        // считаем сопротивление
+        let mut damage = hit.damage;
+        resistance.calculate_for(&mut damage, hit.element);
 
-            //i-frames
-            commands
-            .entity(player_e)
-            .insert(Invincibility::new(player.invincibility_time));
+        // наносим урон
+        health.damage(damage);
+
+        //i-frames
+        commands
+        .entity(player_e)
+        .insert(Invincibility::new(player.invincibility_time));
 
 
-            // шлём ивент смерти
-            if health.current <= 0 {
-                // события "поле смерти"
-                ev_death.send(PlayerDeathEvent (player_e));
-            }
+        // шлём ивент смерти
+        if health.current <= 0 {
+            // события "поле смерти"
+            ev_death.send(PlayerDeathEvent (player_e));
         }
     }
 }
