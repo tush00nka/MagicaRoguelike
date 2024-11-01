0db97c945a50a4bd24e6de7ffeb7459bb8627524
tush00nka <endline00@ya.ru>
2024-11-02 00:10:44 +0300

    hp bar's number display fixed

--- src/ui/health_ui.rs
+++ src/ui/health_ui.rs
@@ -38,6 +38,7 @@ fn spawn_ui(
         },
         ..default()
     })
+    .insert(HPBarUI)
     .with_children(|parent| {
         parent.spawn(ImageBundle { // фон полоски ХП
             image: UiImage::solid_color(Color::hsl(0.0, 1.0, 0.1)),
@@ -51,7 +52,6 @@ fn spawn_ui(
             },
             ..default()
         })
-        .insert(HPBarUI)
         .with_children(
             |parent| { // сама полоска ХП
                 parent.spawn(ImageBundle {
@@ -67,21 +67,7 @@ fn spawn_ui(
                     },
                     ..default()
                     })
-                    .insert(HPBar)
-                    .with_children(|parent| {
-                        parent.spawn(TextBundle {
-                            text: Text::from_section(
-                                "100/100",
-                                TextStyle {
-                                    font: asset_server.load("fonts/ebbe_bold.ttf"),
-                                    font_size: 16.0,
-                                    color: Color::WHITE,
-                                    ..default()
-                                }),
-                            ..default()
-                        })
-                        .insert(HPText);
-                    }); 
+                    .insert(HPBar); 
             }
         );
 
@@ -98,6 +84,32 @@ fn spawn_ui(
         })
         .insert(ExtraLivesText);
     });
+
+    commands.spawn(TextBundle {
+        style: Style {
+            width: Val::Px(192.),
+            height: Val::Px(24.),
+            left: Val::Px(0.),
+            top: Val::Px(24.),
+            ..default()
+        },
+        text: Text {
+            sections: vec![TextSection {
+                value: "100/100".to_string(),
+                style: TextStyle {
+                    font: asset_server.load("fonts/ebbe_bold.ttf"),
+                    font_size: 16.0,
+                    color: Color::WHITE,
+                    ..default()
+                }
+            }],
+            justify: JustifyText::Center,
+            ..default()
+        },
+        ..default()
+    })
+    .insert(HPBarUI)
+    .insert(HPText);
 }
 
 fn update_ui(
