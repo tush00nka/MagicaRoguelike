74f3eb37c511d6a46b742d8d678bd51b8b2d1d7a
tush00nka <endline00@ya.ru>
2024-11-25 23:12:10 +0300

    saving unlock progress

--- src/save.rs
+++ src/save.rs
@@ -1,6 +1,10 @@
 use bevy::prelude::*;
 use bevy_common_assets::json::JsonAssetPlugin;
 
+use std::{fs::File, io::Write};
+
+use crate::GameState;
+
 pub struct SavePlugin;
 
 impl Plugin for SavePlugin {
@@ -8,11 +12,12 @@ impl Plugin for SavePlugin {
         app.add_plugins(JsonAssetPlugin::<Save>::new(&["json"]));
 
         app.add_systems(PreStartup, load);
+        app.add_systems(OnEnter(GameState::GameOver), save);
         
     }
 }
 
-#[derive(serde::Deserialize, Asset, TypePath)]
+#[derive(serde::Deserialize, serde::Serialize, Asset, TypePath)]
 pub struct Save {
     pub seen_items: Vec<String>,
     pub seen_mobs: Vec<String>,
@@ -28,4 +33,16 @@ fn load(
 ) {
     println!("asasd");
     commands.insert_resource(SaveHandle(asset_server.load("save.json")));
+}
+
+fn save(
+    saves: Res<Assets<Save>>,
+    handle: Res<SaveHandle>,
+) {
+    let save = saves.get(handle.0.id()).unwrap();
+
+    let json_string = serde_json::to_string(save).expect("[E] Couldn't save to JSON!!");
+
+    let mut save_file = File::create("assets/save.json").expect("[E] Couldn't open save file!!");
+    save_file.write_all(json_string.as_bytes()).expect("[E] Couldn't write to file!!");
 }
